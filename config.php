f9df092caecbf65e647dc53a0c0465c1012bda36
almarid <ebrahim1992itt@gmail.com>
2025-05-15 17:47:16 +0300

    Update config.php
    define('BOT_TOKEN', '7623523587:AAEkmroKyKMZTBolkUolHifxSu9adtln5Rw');

--- config.php
+++ config.php
@@ -4,7 +4,6 @@
 define('BOT_TOKEN', '7623523587:AAEkmroKyKMZTBolkUolHifxSu9adtln5Rw');
 define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
 define('ADMIN_ID', 7553454385); // ← معرف الأدمن تم إدخاله بنجاح
-
 function sendMessage($chat_id, $text, $params = []) {
     $data = array_merge([
         'chat_id' => $chat_id,
@@ -15,6 +14,4 @@ function sendMessage($chat_id, $text, $params = []) {
     $response = file_get_contents(API_URL . 'sendMessage?' . http_build_query($data));
     return json_decode($response, true);
 }
-
 ?>
-
